11945cc35823a15857f138cf9dbea6abd8efd54f
wangxinglong <hacke2@qq.com>
2014-11-16 11:41:20 +0800

    :100: chapter 08 new

--- chapter08/worker.js
+++ chapter08/worker.js
@@ -2,8 +2,8 @@ var fibonacci =function(n) {
     return n <2? n : arguments.callee(n -1) + arguments.callee(n -2);
 };
 //接受主线程的传参
-var onmessage =function(event) {
+self.onmessage =function(event) {
 	var n = parseInt(event.data, 10);
 	//向主线程传递返回结果
-	    postMessage(fibonacci(n));
+	postMessage(fibonacci(n));
 };
